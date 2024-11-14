0dbf9f5a19e3ce9688f6ee1c8becac5c8565e026
Hugo Peek <hugo@fractal-farming.com>
2024-11-14 23:08:31 +0800

    Load GeoJSON data in StoryMap chapters
    If available..

--- core/components/reframebrain/elements/chunks/m2_molecules/m2_map/reframestorymapchapterjs.chunk.js
+++ core/components/reframebrain/elements/chunks/m2_molecules/m2_map/reframestorymapchapterjs.chunk.js
@@ -1,9 +1,20 @@
-L.marker([ [[+Location_lat]], [[+Location_lng]] ]).addTo(map);
+let marker_[[+id]] = null;
+let geoJSON_[[+id]] = null;
+
+[[+Location_geojson:isnot=``:then=`
+geoJSON_[[+id]] = L.geoJSON([[+Location_geojson:replace=`[[==[ [`:replace=`]]==] ]`]]).addTo(map);
+`:else=`
+marker_[[+id]] = L.marker([ [[+Location_lat]], [[+Location_lng]] ]).addTo(map);
+`]]
 
 $('#chapter-[[+id]]').visibility({
     once: false,
     onBottomVisible: function () {
-        map.flyTo([ [[+Location_lat]], [[+Location_lng]] ], [[+Location_zoom:default=`14`]]);
+        if (geoJSON_[[+id]] !== null) {
+            map.flyToBounds(geoJSON_[[+id]].getBounds(), [[+Location_zoom:default=`14`]]);
+        } else {
+            map.flyTo([ [[+Location_lat]], [[+Location_lng]] ], [[+Location_zoom:default=`14`]]);
+        }
         $(this).addClass('active');
     },
     onTopVisibleReverse: function () {
